e5613b40850b6200f2779ac0b98b2877ed61f407
Maksim Semenov <maksimsemenov@Maksims-MacBook-Air.local>
2021-02-09 10:44:41 -0500

    Remove build artefacts

--- dist/moduleA.js
+++
@@ -1,2 +0,0 @@
-setInterval((()=>{var e;e="Module A",console.log(e)}),1e3);
-//# sourceMappingURL=moduleA.js.map
--- dist/moduleB.js
+++
@@ -1,2 +0,0 @@
-setInterval((()=>{var e;e="Module B",console.log(e)}),1e3);
-//# sourceMappingURL=moduleB.js.map
